59f7f12ec29ac7a10a61d766962b0b13f13f7262
Eleanor <e.e.j@live.co.uk>
2019-12-09 16:17:34 +0000

    ping pong app with previous results table, scoring, switching servers with every 5 points

--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import { createStore, compose } from "redux";
+import persistState from "redux-localstorage";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initial = {
+    player1: 0,
+    player2: 0,
+    serve: 0,
+    winner: 0,
+    previous: []
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state, action) => {
+
+    const serving = state => {
+        let alt = state.player1 < 21 || state.player2 < 21 ? 5 : 2;
+
+        return {
+            ...state, 
+            serve: Math.floor((state.player1 + state.player2 + 1) / alt) % 2 + 1
+        }
+    }
+
+    const winner = state => {
+        if ((state.player1 >= 21 || state.player2 >= 21) && (Math.abs(state.player1 - state.player2) >= 2)) {
+            console.log(state.previous); 
+            return {
+                ...state, 
+                winner: state.player1 > state.player2 ? 1 : 2,
+            }
+        } else {
+            return {
+                ...state,
+                winner: 0,
+            }
+        }
+    }
+ 
+   switch (action.type) {
+    case "incrementP1": return winner(serving({ ...state, 
+        player1: state.player1 + 1, 
+    })); 
+    case "incrementP2": return winner(serving({ ...state, 
+        player2: state.player2 + 1, 
+    }));
+    case "reset": 
+    
+    return { ...initial, previous: [...state.previous, state] };
+
+    default: return state;
+ } };
+
+const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    initial,
+    composeEnhancers(persistState())
+);
+
+const render = () => {
+    let state = store.getState();
+
+ // pass in state.value as a value prop
+    ReactDOM.render(
+      <App 
+        player1={ state.player1 } 
+        player2={ state.player2 }
+        handleIncrementP1={  () => store.dispatch({ type: "incrementP1" }) }
+        handleIncrementP2={  () => store.dispatch({ type: "incrementP2" }) }
+        handleReset={  () => store.dispatch({ type: "reset" })  }
+        serving={ state.serve }
+        winner={ state.winner }
+        previous={ state.previous }
+    />,
+
+      document.getElementById("root")
+  ); };
+
+  store.subscribe(render); // render when state changes
+  render(); // render when page loads using initial state
+
