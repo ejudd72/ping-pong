a24076f1a0007d25759a06d201e4306d95bbf360
Eleanor <e.e.j@live.co.uk>
2020-01-06 09:21:14 +0000

    Updates

---
+++ precache-manifest.cfa5c9a7e613644fb37834f01b314c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77484fbb80634767b8e64b962327935",
+    "url": "/ping-pong/index.html"
+  },
+  {
+    "revision": "b6130161aa26be2f659d",
+    "url": "/ping-pong/static/js/2.873b029f.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/ping-pong/static/js/2.873b029f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "347062b36a00b2536077",
+    "url": "/ping-pong/static/js/main.6942eae6.chunk.js"
+  },
+  {
+    "revision": "4fd0ecf3145c494b6ea0",
+    "url": "/ping-pong/static/js/runtime-main.6d3f71f4.js"
+  }
+]);
